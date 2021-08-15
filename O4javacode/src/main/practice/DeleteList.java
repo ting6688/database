4a09d1d6c47a3800ca9470edd40a07774ef8b71d
zhangting <1510079209@qq.com>
2021-08-15 21:50:08 +0800

    feat:deleteList

---
+++ O4javacode/src/main/practice/DeleteList.java
@@ -0,0 +1,58 @@
+package practice;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * 删除顺序结构线性表中的元素
+ */
+public class DeleteList {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>(16);
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+        list.add("5");
+        list.add("6");
+        System.out.println("before delete:"+list);
+        delete(list,3);
+        System.out.println("after delete:"+list);
+
+
+
+
+    }
+
+
+    /**
+     * 删除list中在index位置的element元素
+     *
+     * @param list
+     * @param index
+     */
+    public static void delete(List<String> list, int index) {
+        if (list.size() == 0) {
+            System.out.println("线性表为空");
+            return;
+        }
+        if (index < 1 || index > list.size()) {
+            System.out.println("删除位置不正确，超范围");
+            return;
+        }
+        // 如果该元素不是最后一个，则删除元素之后的位置都要往前移一个位置
+        if (Objects.equals(list.size(), index)) {
+            // 是最后一个直接删除
+            list.remove(index - 1);
+            return;
+        }
+        // before delete:[1, 2, 3, 4, 5, 6]
+        // after delete:[1, 2, 4, 5, 6, 6]
+        for (int i = index; i < list.size(); i++) {
+            list.set(i - 1, list.get(i));
+        }
+        return;
+    }
+}

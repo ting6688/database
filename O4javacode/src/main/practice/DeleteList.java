2653743addcec84c5818057a17213c86ec6691a7
zhangting <1510079209@qq.com>
2021-08-15 23:12:42 +0800

    feat:deleteArray

--- O4javacode/src/main/practice/DeleteList.java
+++ O4javacode/src/main/practice/DeleteList.java
@@ -2,6 +2,7 @@ package practice;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
@@ -10,16 +11,34 @@ import java.util.Objects;
  */
 public class DeleteList {
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>(16);
-        list.add("1");
-        list.add("2");
-        list.add("3");
-        list.add("4");
-        list.add("5");
-        list.add("6");
-        System.out.println("before delete:" + list);
-        delete(list, 3);
-        System.out.println("after delete:" + list);
+        Integer[] arr1 = new Integer[12];
+        arr1[0] = 1;
+        arr1[1] = 2;
+        arr1[2] = 3;
+        arr1[3] = 4;
+        arr1[4] = 5;
+        arr1[5] = 6;
+        arr1[6] = 7;
+        arr1[7] = 8;
+
+//        System.out.println(arr1.toString());
+//        System.out.println("length:"+ arr1.length);
+//
+//        System.out.println("arrget:"+ arr1[8] +",arr9:"+arr1[9]);
+//
+//        List<String> list = new ArrayList<>(16);
+//        list.add("1");
+//        list.add("2");
+//        list.add("3");
+//        list.add("4");
+//        list.add("5");
+//        list.add("6");
+        // 数组的length为final 不可变，数组没有删除方法
+        //before delete:[1, 2, 3, 4, 5, 6, 7, 8, null, null, null, null] length 12
+       //  after delete:[1, 2, 4, 5, 6, 7, 8, null, null, null, null, null] length 12
+        System.out.println("before delete:" + Arrays.toString(arr1) + ",length:"+arr1.length);
+        delete(arr1, 3);
+        System.out.println("after delete:" + Arrays.toString(arr1) + ",length:"+arr1.length);
 
 
     }
@@ -31,12 +50,12 @@ public class DeleteList {
      * @param list
      * @param index
      */
-    public static void delete(List<String> list, int index) {
-        if (list.size() == 0) {
+    public static void delete(Integer[] list, int index) {
+        if (list.length == 0) {
             System.out.println("线性表为空");
             return;
         }
-        if (index < 1 || index > list.size()) {
+        if (index < 1 || index > list.length) {
             System.out.println("删除位置不正确，超范围");
             return;
         }
@@ -51,10 +70,10 @@ public class DeleteList {
         // 删除元素，先删除，再移动位置
         // error: before delete:[1, 2, 3, 4, 5, 6] ,after delete:[1, 2, 5, 6, 6];
         // 因为arrayList数组本身remove造成的这个原因
-        list.remove(index - 1);
-        if (!Objects.equals(list.size(), index)) {
-            for (int i = index; i < list.size(); i++) {
-                list.set(i - 1, list.get(i));
+
+        if (!Objects.equals(list.length, index)) {
+            for (int i = index; i < list.length; i++) {
+                list[i-1] = list[i];
             }
         }
         return;
